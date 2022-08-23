b867c7dc4511cc35a200e7999bad7196c7680d79
Arvinder Shinh <jollyshinh@yahoo.co.in>
2022-08-23 18:28:04 +0530

    del

--- dummyModel.py
+++
@@ -1,9 +0,0 @@
-import os
-import matplotlib.image as mpimg
-from recom import get_recommendations
-
-def recommendItem(input_image_list, caption):
-
-    print("-----------------recommendItem called---------------")
-    output_image_list = get_recommendations(input_image_list, caption)
-    return output_image_list
